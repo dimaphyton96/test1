6806b1ae74fb53ba7e2b35afa7bd814d6c780b9b
Ceban Dmitrii <cebandima1996@gmai.com>
2023-02-12 20:04:14 +0300

    add program

--- test_1.py
+++ test_1.py
@@ -1,14 +1,27 @@
-a = 9
-b = 2
-print(a * b)
+while True:
+    comand = input('Введите операцию: ')
+    if comand in '+-*/':
+        break
+    print('Ошибка, такой операции не существует. Попробуйте еще раз!')
 
+count = 1
+number = int(input(f'Введите число {count}: '))
+result_str = str(number)
+result = number
+while number != 0:
+    count += 1
+    number = int(input(f'Введите число {count}'))
 
+    if number != 0:
+        if comand == '+':
+            result += number
+        elif comand == '-':
+            result -= number
+        elif comand == '*':
+            result *= number
+        elif comand == '/':
+            result /= number
+    result_str += ' ' + comand + ' ' + str(number)
 
+print(result_str + ' = ' + str(result))
 
-<<<<<<< HEAD
-some chenges 
-=======
-some chenges bla bla bla bla bla bla 
-
-add changes
->>>>>>> f5b2a10d013369429f8147bd113aae1a75d18b5c
